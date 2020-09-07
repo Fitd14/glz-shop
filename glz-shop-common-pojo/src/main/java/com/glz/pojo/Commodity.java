3f290d43108b41d70f5294b7c867621bd384fbae
Dsj <15938652683@163.com>
2020-09-07 13:47:05 +0800

    购物车

--- glz-shop-common-pojo/src/main/java/com/glz/pojo/Commodity.java
+++ glz-shop-common-pojo/src/main/java/com/glz/pojo/Commodity.java
@@ -3,7 +3,6 @@ package com.glz.pojo;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-import com.sun.istack.internal.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 

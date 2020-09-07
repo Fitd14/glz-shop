9df515fae2957ca9fb869a17bfb673bea38e78a8
baibai <bai_742@163.com>
2020-09-07 15:29:37 +0800

    更新，commodity，pom等

---
+++ glz-shop-commodity/src/main/java/com/smy/shop/mapper/CommodityMapper.java
@@ -0,0 +1,16 @@
+package com.smy.shop.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.glz.pojo.Commodity;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface CommodityMapper extends BaseMapper<Commodity> {
+
+    List<Commodity> getByOther(@Param("commodityName") String commodityName, @Param("commoditySubHead") String commoditySubHead, @Param("brand") String brand);
+
+    List<Commodity> selectAll();
+}

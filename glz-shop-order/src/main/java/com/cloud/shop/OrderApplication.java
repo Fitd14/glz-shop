cc9bf5349798c4c420e3f8b0008a48f90762974e
chenyunpeng <chyunp@aliyun.com>
2020-09-08 15:55:28 +0800

    添加order跨域配置

--- glz-shop-order/src/main/java/com/cloud/shop/OrderApplication.java
+++ glz-shop-order/src/main/java/com/cloud/shop/OrderApplication.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @SpringBootApplication
 @EnableDiscoveryClient
 @MapperScan("com.cloud.shop.mapper")
-//@CrossOrigin // 声明跨域
+@CrossOrigin // 声明跨域
 public class OrderApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication.class, args);

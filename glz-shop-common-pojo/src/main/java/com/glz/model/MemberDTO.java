5c1af23176f1ac145d3568de786cfcda204a4f35
simingyang <15225407491@163.com>
2020-09-15 18:07:08 +0800

    fix member

--- glz-shop-common-pojo/src/main/java/com/glz/model/MemberDTO.java
+++ glz-shop-common-pojo/src/main/java/com/glz/model/MemberDTO.java
@@ -5,13 +5,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
 import java.util.Set;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class MemberDTO {
+
+    private String userId;
 
     private String username;
 

841b153817e88811786df180e410cfdbfed84db9
simingyang <15225407491@163.com>
2020-09-17 08:45:31 +0800

    fix

--- glz-shop-order/src/main/java/com/cloud/shop/controller/FileUploadController.java
+++ glz-shop-order/src/main/java/com/cloud/shop/controller/FileUploadController.java
@@ -1,19 +1,23 @@
-package com.cloud.shop.mapper;
+package com.cloud.shop.controller;
 
 import com.cloud.smy.service.FileUploadService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-@RestController("/file")
+@RestController
+@RequestMapping("/orderFile")
 public class FileUploadController {
     @Autowired
     FileUploadService fileUploadService;
 
-    @RequestMapping("/upload")
+    @PostMapping("/upload")
     public String upload(MultipartFile file) {
         return fileUploadService.fileUpload(file);
     }
 
+    @GetMapping("/hello")
+    public String getHello(){
+        return "hello,你好吗，我很好,iamfine";
+    }
 }
